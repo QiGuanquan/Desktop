2d5b7ecf2a65685d5854490d684f2583818a3113
dodo <hollykunge@gmail.com>
2020-02-15 21:35:09 +0800

    统一地址到配置文件

--- src/components/LarkPage.vue
+++ src/components/LarkPage.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="lark-frame" id="larkFrame">
     <vue-element-loading :active="isShow" spinner="spinner" color="#FF6700" size="64"/>
-        <webview :v-show="!isShow" id="larkPage" src="http://127.0.0.1:8081" autosize="on" style="display:flex; height:100%; width:100%;"
+        <webview :v-show="!isShow" id="larkPage" :src="src" autosize="on" style="display:flex; height:100%; width:100%;"
              minwidth="1024" minheight="768" partition="trusted"></webview>
   </div>
 </template>
 
 <script>
+import { App } from 'nw.gui'
 import {createBrowser} from '@/utils/browser'
 import {createNotification} from '@/utils/notification'
 import {handleRequest} from '@/utils/permissionrequest'
 import { checkNetWork } from '@/utils/network.js'
 import VueElementLoading from 'vue-element-loading'
+const { manifest } = App
 
 export default {
   name: 'LarkPage',
@@ -22,7 +24,8 @@ export default {
       isLoading: true,
       isDebug: false,
       mainWebView: {},
-      isShow: true
+      isShow: true,
+      src: manifest.webviewSrc
     }
   },
   created () {
